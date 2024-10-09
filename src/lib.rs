dfbba43444566aadea7278c8091d3861ecb62377
Reece Harris <reece@codegalaxy.co.uk>
2024-10-09 16:42:38 +0100

    Refactor HTTP server to support HTTP/2

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::convert::Infallible;
 use std::net::SocketAddr;
 use std::time::{SystemTime, UNIX_EPOCH};
 use hyper::body::Bytes;
-use hyper::server::conn::http1;
+use hyper::server::conn::{http1, http2};
 use hyper::service::service_fn;
 use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
@@ -118,7 +118,7 @@ impl Server {
 
             // Spawn a new task for each connection
             tokio::task::spawn(async move {
-                if let Err(err) = http1::Builder::new()
+                if let Err(err) = http1::Builder::new(/* exec */)
                     .serve_connection(io, service_fn(Self::index))
                     .await
                 {
