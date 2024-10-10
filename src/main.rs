90a36c1134bf45ba8ad030a62fbe6e00b2743e4f
Reece Harris <reece@codegalaxy.co.uk>
2024-10-10 10:38:08 +0100

    Refactor server to add process function and support HTTP/2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-use pterodactyl::Server;
+use pterodactyl::{Server, ProcessResponse};
 use std::net::SocketAddr;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
     let server = Server::new()?;
 
-    server.start(addr).await
+    server.add_process("example", || {
+
+        let response = ProcessResponse::new(
+            200,
+            "Success".to_string(),
+            serde_json::json!({
+                "message": "Hello, World!",
+                "data": {
+                    "key": "value"
+                }
+            }),
+            [].to_vec(),
+        );
+
+        Ok(response)
+    });
+
+    Arc::new(server).start(addr).await
 }
